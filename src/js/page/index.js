f26e1bf2997cacbe6b9d66f3e941b47d63308c1b
chenyinlong <985350503@qq.com>
2017-07-27 18:17:17 +0800

    Signed-off-by: chenyinlong <985350503@qq.com>

---
+++ src/js/page/index.js
@@ -0,0 +1,11 @@
+// 引入css
+require('../../css/normalize.css')
+require('../../css/global.css')
+
+require('../../css/amazeui.min.css')
+require('../../css/page/index.css')
+
+require('../amazeui.min.js')
+require('../../view/index.html') // 热加载 生产时去掉
+
+
