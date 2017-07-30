b9d3636651b93e5754b622d3f3f69891a6f96aa3
chenyinlong <985350503@qq.com>
2017-07-30 21:30:19 +0800

    联系我们 page

---
+++ src/js/contact.js
@@ -0,0 +1,13 @@
+
+// 引入css
+require('../css/global.css')
+require('../css/amazeui.min.css')
+
+require('../css/header.css')
+require('../css/footer.css')
+require('../css/other_static.css')
+require('../css/contact.css')
+
+require('../js/lib/amazeui.min')
+require('../view/contact.html') // 热加载 生产时去掉
+
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -17,7 +17,8 @@ module.exports = {
   entry: {
     index: './src/js/index.js',
     about: './src/js/about.js',
-    product: './src/js/product.js'
+    product: './src/js/product.js',
+    contact: './src/js/contact.js'
   },
   output: {
     path: path.join(__dirname, 'dist'), // 输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
@@ -60,7 +61,7 @@ module.exports = {
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendors', // 将公共模块提取，生成名为`vendors`的chunk
-      chunks: ['index', 'list', 'about','product'], // 提取哪些模块共有的部分
+      chunks: ['index', 'list', 'about','product','contact'], // 提取哪些模块共有的部分
       minChunks: 3 // 提取至少3个模块共有的部分
     }),
     new ExtractTextPlugin('css/[name].css'), // 单独使用link标签加载css并设置路径，相对于output配置中的publickPath
@@ -97,6 +98,19 @@ module.exports = {
       inject: 'body', // js插入的位置，true/'head'/'body'/false
 
       chunks: ['vendors', 'product'], // 需要引入的chunk，不配置就会引入所有页面的资源
+      minify: { // 压缩HTML文件
+        removeComments: true, // 移除HTML中的注释
+        collapseWhitespace: false // 删除空白符与换行符
+      }
+    }),
+
+    new HtmlWebpackPlugin({ // 根据模板插入css/js等生成最终HTML
+      favicon: './src/img/favicon.ico', // favicon路径，通过webpack引入同时可以生成hash值
+      filename: './contact.html', // 生成的html存放路径，相对于path
+      template: './src/view/contact.html', // html模板路径
+      inject: 'body', // js插入的位置，true/'head'/'body'/false
+
+      chunks: ['vendors', 'contact'], // 需要引入的chunk，不配置就会引入所有页面的资源
       minify: { // 压缩HTML文件
         removeComments: true, // 移除HTML中的注释
         collapseWhitespace: false // 删除空白符与换行符
